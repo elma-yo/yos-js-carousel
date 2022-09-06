d31ee7b59b90900885638e52dc5ce60de3d5290d
~elma-yo~ <~elmaspy@gmail.com~>
2022-09-06 17:50:05 +0900

    丸いボタンウィ動的に生成する

--- js/main.js
+++ js/main.js
@@ -5,9 +5,10 @@
     const prev = document.getElementById('prev');
     const ul = document.querySelector('ul');
     const slides = ul.children;
+    const dots = [];
     let currentIndex = 0;
 
-    function updateButtons () {
+    function updateButtons() {
         prev.classList.remove('hidden');
         next.classList.remove('hidden');
 
@@ -19,12 +20,23 @@
         }
     }
 
-    function moveSlides () {
+    function moveSlides() {
         const slideWidth = slides[0].getBoundingClientRect().width;
         ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
     }
 
+    function setupDots() {
+        for (let i = 0; i < slides.length; i++) {
+            const button = document.createElement('button');
+            dots.push(button);
+            document.querySelector('nav').appendChild(button);
+        }
+
+        dots[0].classList.add('current');
+    }
+
     updateButtons();
+    setupDots();
 
     next.addEventListener('click', () => {
         currentIndex++;
